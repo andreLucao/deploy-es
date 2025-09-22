731fd228bc08e75269db67c53729f3050c761e11
andreLucao <luccadeandrade11@gmail.com>
2025-09-21 23:34:56 -0300

    merge de db_connection com develop e rotas somente dentro do app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,21 @@
-import express from "express";
+import express, { Application } from "express";
+import basicRoutes from "./routes/basic.routes";
+import authRoutes from "./routes/authRoutes";
 import calculatorRoutes from "./routes/calculator.routes";
 import emissionProductsRoutes from "./routes/emission_products.routes";
 import emissionFactorsRoutes from "./routes/emission_factors.routes";
 import companiesRoutes from "./routes/companies.routes";
 
-const app = express();
+const app: Application = express();
 export const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Routes
+app.use("/", basicRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/calculator", calculatorRoutes);
 app.use("/api/emission-products", emissionProductsRoutes);
 app.use("/api/emission-factors", emissionFactorsRoutes);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,4 @@
 import app, { PORT } from "./app";
-import authRoutes from "./routes/authRoutes";
-
-// Mount auth routes
-app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
    console.log(`Server is running at http://localhost:${PORT}`);
