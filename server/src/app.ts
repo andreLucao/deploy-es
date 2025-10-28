4a3591eb62216eac1643ab59f25d26abb688baa1
João Vitor <joaovitor20022004@gmail.com>
2025-10-28 11:38:53 -0300

    History route and service created

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,19 +8,29 @@ import calculatorRoutes from "./routes/calculator.routes";
 import emissionProductsRoutes from "./routes/emission_products.routes";
 import emissionFactorsRoutes from "./routes/emission_factors.routes";
 import companiesRoutes from "./routes/companies.routes";
-import adProductRoutes from './routes/adProduct.routes';
+import adProductRoutes from "./routes/adProduct.routes";
 import paymentRoutes from "./routes/paymentRoutes";
 import stripeWebhook from "./routes/stripeWebhook";
 import commentsRoutes from "./routes/commentsRoutes";
 import productsRoutes from "./routes/products.routes";
+import transactionRoutes from "./routes/transaction.routes";
 
 const app: Application = express();
 export const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:3000", credentials: true }));
+app.use(
+   cors({
+      origin: process.env.FRONTEND_URL || "http://localhost:3000",
+      credentials: true,
+   })
+);
 
-app.use("/api/stripe/webhook", express.raw({ type: 'application/json' }), stripeWebhook);
+app.use(
+   "/api/stripe/webhook",
+   express.raw({ type: "application/json" }),
+   stripeWebhook
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -33,9 +43,10 @@ app.use("/api/calculator", calculatorRoutes);
 app.use("/api/emission-products", emissionProductsRoutes);
 app.use("/api/emission-factors", emissionFactorsRoutes);
 app.use("/api/companies", companiesRoutes);
-app.use('/api/adProducts', adProductRoutes);
+app.use("/api/adProducts", adProductRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/comments", commentsRoutes);
 app.use("/api/products", productsRoutes);
+app.use("/api/transactions", transactionRoutes);
 
 export default app;
