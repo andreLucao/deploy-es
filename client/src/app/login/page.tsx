24237b90f0145f385f5f13df1d00f179363e3c0d
JollonLD <jonas.durao@unifesp.br>
2025-11-05 14:28:59 -0300

    correções carrinho

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -3,9 +3,9 @@ import LoginCard from "@/components/login/LoginCard";
 
 export default function LoginPage() {
     return (
-        <main className="bg-gradient-to-tr from-[#002e34] to-[#00e07f] w-full min-h-screen">
-            <div className="flex spacing-responsive-md min-h-screen items-center justify-center bg-gradient-to-tr from-[#002e34] to-[#00e07f]">
-                <div className="w-full max-w-md mx-auto">
+        <main className="bg-gradient-to-tr from-[#002e34] to-[#00e07f] w-full min-h-screen overflow-x-hidden">
+            <div className="flex min-h-screen items-center justify-center px-4 py-8 sm:py-12">
+                <div className="w-full max-w-sm sm:max-w-2xl lg:max-w-7xl">
                     <LoginCard/>
                 </div>
             </div>
